adc3316d1ab3c2bfb82a8fbefa20428cff616fc8
sabrina880505 <55083125+sabrina880505@users.noreply.github.com>
2020-04-17 11:51:01 +0800

    Update BMI.py

--- week6/BMI.py
+++ week6/BMI.py
@@ -18,5 +18,6 @@ elif 18.5<= bmi <25:
   print("身高{}公分、體重{}公斤、是Normal weight".format(height,weight))
 elif 25<= bmi <30:
   print("身高{}公分、體重{}公斤、是Over weight".format(height,weight))
+else:
   print("身高{}公分、體重{}公斤、是Obese".format(height,weight))
 
